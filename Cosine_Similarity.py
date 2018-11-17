5ed89cad2dfbe448def27fbddf98d969803e3f47
John Benhart <jbb58@duke.edu>
2018-11-17 12:46:22 -0500

    cosine similarity between books is working; need to integrate with all text files in current framework, working on run-time now

--- Cosine_Similarity.py
+++ Cosine_Similarity.py
@@ -1,18 +1,26 @@
 import nltk
+import math
+import os
 def dot(dict1,dict2):
         ret = 0
         if(len(dict1)<=len(dict2)):
             for key in dict1.keys():
                 if(key in dict2):
-                    ret += dict1[key]["tfidf"]*dict2[key]["tfidf"]
+                    ret += dict1[key][1]*dict2[key][1]
         else:
             for key in dict2.keys():
                 if(key in dict1):
-                    ret += dict1[key]["tfidf"]*dict2[key]["tfidf"]
+                    ret += dict1[key][1]*dict2[key][1]
         return ret
-set_words = set([])
+import os
+f_list = []
+for file in os.listdir("./"):
+    if file.endswith(".txt"):
+        f_list+=[file]
 #for file in directory:
-for file in ["gulliver.txt"]:
+dictDF = {}
+books = {}
+for file in f_list:
     #create text's dictionaries
     dictWords = {} #words to occurences
     #read in file
@@ -24,40 +32,77 @@ for file in ["gulliver.txt"]:
     while (begin < len(text) and indic):
         line = text[begin]
         begin += 1
+        if(len(line)>7):
+            if(line[0:6]=="Title:"):
+                sep = line.split(" ")
+                sep[-1] = sep[-1][0:-1]
+                title = tuple(sep[1:])
+                print(title)
+                books[file] = {}
+                books[file]["title"] = title
+            if(line[0:7]=="Author:"):
+                sep = line.split(" ")
+                sep[-1] = sep[-1][0:-1]
+                author = tuple(sep[1:])
+                print(author)
+                books[file] = {}
+                books[file]["author"] = author
         if(len(line)>8):
             if(line[0:8] == "***START"):
                 indic = False
     book = text[begin:]
     #get all words in text
-    stopTags = set(["CC","CD","DT","IN", "LS", "MD", "PDT", "POS", "PRP$", "TO","EX","UH"])
+    #stopTags = set(["CC","CD","DT","IN", "LS", "MD", "PDT", "POS", "PRP$", "TO","EX","UH"])
     #SHOULD GET RID OF STOPWORDS.NLTK FOR OVERALL STATS
-    stopWords = set(nltk.corpus.stopwords.words("english")).union(set(["-","’","‘", "_",";","(",")","<",">",",","''","``","”",'“',".","?",":","%",", "," ","n","=",",  ","#","$","@","{","}","[","]"]))
-    for line in text:
+    #stopWords = set(nltk.corpus.stopwords.words("english")).union(set(["-","’","‘", "_",";","(",")","<",">",",","''","``","”",'“',".","?",":","%",", "," ","n","=",",  ","#","$","@","{","}","[","]"]))
+    stopWords = set(["-","’","‘", "_",";","(",")","<",">",",","''","``","”",'“',".","?",":","%",", "," ","n","=",",  ","#","$","@","{","}","[","]"])
+    for line in book:
         tokens = nltk.word_tokenize(line)
-        #SHOULD PROBABLY STEM HERE, FOR MOST DOCUMENT COMPARISON
+        #SHOULD PROBABLY STEM HERE, FOR MOST DOCUMENT COMPARISON; COULD STEM FOR SIMILARITY BUT NOT FOR COMMON WORDS
         #CAN ELIMINATE TOO COMMON WORDS: lines = [word[0].lower() for word in nltk.pos_tag(tokens) if word[1] not in stopTags and word[0] not in stopWords]
         refined = [word[0].lower() for word in nltk.pos_tag(tokens) if word[0] not in stopWords]
         for word in refined:
             if(word not in dictWords):
                 dictWords[word] = [1,0] #first is tf, second is tf*idf
+                if(word not in dictDF):
+                    dictDF[word] = 1
+                else:
+                    dictDF[word] += 1
             else:
                 dictWords[word][0] +=1
-            set_words.add(word)
-for item in sorted(dictWords.items(),key = lambda ite: ite[1][0]):
-    print(item)
-    #put dictWords into a library by title
+    books[file]["word_freq"] = dictWords
+#put dictWords into a library by title
 #create IDF
-# dictIDF = {}
-# for word in list(set_words):
-#     for book in library:
-#     if(word in text)
+dictIDF = {}
+for word in dictDF:
+    dictIDF[word] = math.log(len(books)/dictDF[word])+1
+for book in books.keys(): #book is such a strange word...2 O's...
+    word_dict = books[book]["word_freq"]
+    for word in word_dict:
+        word_dict[word][1] = word_dict[word][0]*dictIDF[word]
+    a = math.sqrt(dot(word_dict,word_dict))
+    books[book]["magnitude"] = a
+    if(a == 0):
+        print("magnitude is 0!!!")
+dictSimilarities = {}
+book_list = list(books.keys())
+iter_books = book_list[0:(len(book_list)//2+1)]
+for book1 in iter_books:
+    w_d1 = books[book1]["word_freq"]
+    for book2 in books:
+        w_d2 = books[book2]["word_freq"]
+        sim = dot(w_d1,w_d2)/(books[book1]["magnitude"]*books[book2]["magnitude"])
+        if(book1 not in dictSimilarities):
+            dictSimilarities[book1] = {}
+        dictSimilarities[book1][book2] = sim
+        if(book2 not in dictSimilarities):
+            dictSimilarities[book2] = {}
+        dictSimilarities[book2][book1] = sim
+for book in books:
+    for book1 in books:
+        print(book+" "+book1+" "+str(dictSimilarities[book][book1]))
 
 
-
-    #while ind < len(text):
-        #need to get rid of
-
-    #parse to the beginning of the book, recording author + date + etc
 
 # def initial_process_files(directory):
 #     for file in directory:
