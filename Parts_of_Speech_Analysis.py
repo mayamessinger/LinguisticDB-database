6d467f09a72d75820f38ceea6aa29d28d7ac73cd
John Benhart <jbb58@duke.edu>
2018-11-19 20:05:39 -0500

    added some useful lines to Parts_of_Speech_Analysis.py

--- Parts_of_Speech_Analysis.py
+++ Parts_of_Speech_Analysis.py
@@ -12,3 +12,24 @@ pos_list = ["CC","CD","DT","EX","FW","IN","JJ","JJR","JJS", "LS","MD","NN","NNS"
                 "PDT","POS","PRP","PRP$","RB","RBR","RBS","RP","TO","UH","VB","VBD","VBG","VBN","VBP", \
                 "VBZ","WDT","WP","WP$","WRB"]
 #definitions of pos codes: https://pythonprogramming.net/natural-language-toolkit-nltk-part-speech-tagging/
+dictPOS = {}
+dictTagSequences = {}
+prev_tag = 0
+for line in book:
+    line = line.split(" ")
+    tokens = []
+    for word in line:
+        word = word.lower()
+        token = nltk.word_tokenize(word)
+        tag = nltk.pos_tag(token)
+        if(tag not in dictPOS):
+            dictPOS[tag] = 0
+        dictPOS[tag] += 1
+        if(prev_tag):
+            if(tag not in dictTagSequences):
+                dictTagSequences[tag] = {}
+                dictTagSequences[tag]["prev"] = {}
+                dictTagSequences[tag]["post"] = {}
+            dictTagSequences[tag]["prev"][prev_tag] +=1
+            dictTagSequences[prev_tag]["post"][tag] +=1
+        prev_tag = tag
